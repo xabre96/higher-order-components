587f1ddfdb33bfd732ca542eece380f0b6f23c39
xabre96 <xan.gutierrez@gmail.com>
2018-03-09 10:33:21 +0800

    Add button component.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,28 +1,11 @@
-import React, { Component } from 'react';
-// import Increment from '../../hoc/Increment';
-//
-// const Button = (props) => {
-//   return (
-//     <button onClick={props.incrementCount}>Count: {props.count}</button>
-//   );
-// }
+import React from 'react';
 
-class Button extends Component {
-  state = {
-    count: 0,
-  }
-
-  incrementCount = () => {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  }
-
-  render() {
-    return (
-      <button onClick={this.incrementCount}>Count: {this.state.count}</button>
-    );
-  }
+const Button = (props) => {
+  return (
+    <button onMouseEnter={props.incrementCountByTwo} onClick={props.incrementCount}>
+      Count: {props.count}
+    </button>
+  );
 }
 
 export default Button;
